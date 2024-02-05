f53e0c816e1e765455f482f8b6f223aeb8d71211
alexyao2015 <alexyao2015@users.noreply.github.com>
2024-02-05 06:34:33 +0000

    Typescript

---
+++ build.mjs
@@ -0,0 +1,23 @@
+import esbuild from "esbuild";
+
+const context = await esbuild.context({
+  entryPoints: ["src/index.ts"],
+  bundle: true,
+  minify: true,
+  outfile: "index.js",
+  legalComments: "none",
+  logLevel: "info",
+  platform: "node",
+});
+
+async function run(args) {
+  if (args.includes("--watch")) {
+    // Enable watch mode
+    await context.watch();
+    return;
+  }
+
+  await context.rebuild();
+}
+await run(process.argv.slice(2));
+process.exit();
---
+++ src/config.ts
@@ -0,0 +1,61 @@
+import Conf from "conf";
+import { config } from "dotenv";
+config();
+
+const ACTUAL_SERVER_URL = process.env.ACTUAL_SERVER_URL || "";
+const ACTUAL_SERVER_PASSWORD = process.env.ACTUAL_SERVER_PASSWORD || "";
+
+const APP_PORT = process.env.APP_PORT || 3000;
+
+const APP_URL = process.env.APP_URL || "http://localhost";
+
+const PLAID_CLIENT_ID = process.env.PLAID_CLIENT_ID || "";
+const PLAID_SECRETS = {
+  development: process.env.PLAID_SECRET_DEVELOPMENT,
+  sandbox: process.env.PLAID_SECRET_SANDBOX,
+  production: process.env.PLAID_SECRET_PRODUCTION,
+};
+
+const PLAID_ENV = process.env.PLAID_ENV || "sandbox";
+const PLAID_PRODUCTS = (process.env.PLAID_PRODUCTS || "transactions").split(
+  ","
+);
+const PLAID_COUNTRY_CODES = (process.env.PLAID_COUNTRY_CODES || "US").split(
+  ","
+);
+const PLAID_LANGUAGE = process.env.PLAID_LANGUAGE || "en";
+
+export function getAppConfigFromEnv() {
+  const appConfig = {
+    APP_PORT,
+    APP_URL,
+    PLAID_CLIENT_ID,
+    PLAID_SECRETS,
+    PLAID_ENV,
+    PLAID_PRODUCTS,
+    PLAID_LANGUAGE,
+    PLAID_COUNTRY_CODES,
+    ACTUAL_SERVER_URL,
+    ACTUAL_SERVER_PASSWORD,
+  };
+
+  // Assert that all required environment variables are set
+  Object.entries(appConfig).forEach(([key, value]) => {
+    if (!value) {
+      throw new Error(`Missing environment variable: ${key}`);
+    }
+  });
+
+  return appConfig;
+}
+
+export function getConf(username) {
+  const appConfig = getAppConfigFromEnv();
+  const key = `${username}_${appConfig.PLAID_ENV}`;
+
+  const tmp = new Conf({
+    configName: key,
+  });
+  tmp.set("user", key);
+  return tmp;
+}
